9b3f65b61a1be7cb6dfad085f5a109223ebe6e68
Shreyas Jayaprakash <126165002+shreyasjp@users.noreply.github.com>
2024-02-18 20:23:09 +0530

    Update DB.sql

--- DB.sql
+++ DB.sql
@@ -1,56 +1,5 @@
--- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
---
--- Host: localhost    Database: teachers
--- ------------------------------------------------------
--- Server version	8.0.32
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `teacher_list`
---
-
-DROP TABLE IF EXISTS `teacher_list`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `teacher_list` (
-  `teacher_id` int NOT NULL,
-  `teacher_name` varchar(255) DEFAULT NULL,
-  `department` varchar(255) DEFAULT NULL,
-  `number_of_duties` int NOT NULL DEFAULT '0',
-  PRIMARY KEY (`teacher_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `teacher_list`
---
-
-LOCK TABLES `teacher_list` WRITE;
-/*!40000 ALTER TABLE `teacher_list` DISABLE KEYS */;
-INSERT INTO `teacher_list` VALUES (1,'John Doe','Mathematics',0),(2,'Jane Smith','Physics',0),(3,'Bob Johnson','Chemistry',0),(4,'Alice Williams','CS',0),(5,'David Brown','Mathematics',0),(6,'Emily Davis','Physics',0),(7,'Michael Wilson','Chemistry',0),(8,'Sara Miller','English',0),(9,'Richard Taylor','Physics',0),(10,'Laura Harris','Mathematics',0),(11,'Daniel Martin','Chemistry',0),(12,'Grace Anderson','English',0),(13,'William Thomas','Mathematics',0),(14,'Olivia Jackson','Physics',0),(15,'Ethan White','Chemistry',0),(16,'Ava Martinez','English',0),(17,'Logan Lee','CS',0),(18,'Sophia Taylor','Physics',0),(19,'Jackson Miller','Chemistry',0),(20,'Lily Harris','English',0),(21,'Benjamin Scott','CS',0),(22,'Chloe Allen','Mathematics',0),(23,'Christopher Hall','Chemistry',0),(24,'Mia Turner','English',0),(25,'Elijah Young','Mathematics',0),(26,'Avery Adams','Physics',0),(27,'Addison Clark','CS',0),(28,'Mason Turner','English',0),(29,'Scarlett Walker','Physics',0),(30,'Lucas Lewis','Mathematics',0),(31,'Hannah Parker','CS',0),(32,'Gabriel Wright','English',0),(33,'Aria Foster','Physics',0),(34,'Leo Morris','CS',0),(35,'Zoe Cooper','CS',0);
-/*!40000 ALTER TABLE `teacher_list` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-02-18 20:12:34
+CREATE DATABASE  IF NOT EXISTS `room_data` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `room_data`;
 -- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
 --
 -- Host: localhost    Database: room_data
@@ -134,7 +83,9 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-02-18 20:12:34
+-- Dump completed on 2024-02-18 20:22:27
+CREATE DATABASE  IF NOT EXISTS `test` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `test`;
 -- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
 --
 -- Host: localhost    Database: test
@@ -197,7 +148,64 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-02-18 20:12:34
+-- Dump completed on 2024-02-18 20:22:27
+CREATE DATABASE  IF NOT EXISTS `teachers` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `teachers`;
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: teachers
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `teacher_list`
+--
+
+DROP TABLE IF EXISTS `teacher_list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `teacher_list` (
+  `teacher_id` int NOT NULL,
+  `teacher_name` varchar(255) DEFAULT NULL,
+  `department` varchar(255) DEFAULT NULL,
+  `number_of_duties` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`teacher_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `teacher_list`
+--
+
+LOCK TABLES `teacher_list` WRITE;
+/*!40000 ALTER TABLE `teacher_list` DISABLE KEYS */;
+INSERT INTO `teacher_list` VALUES (1,'John Doe','Mathematics',0),(2,'Jane Smith','Physics',0),(3,'Bob Johnson','Chemistry',0),(4,'Alice Williams','CS',0),(5,'David Brown','Mathematics',0),(6,'Emily Davis','Physics',0),(7,'Michael Wilson','Chemistry',0),(8,'Sara Miller','English',0),(9,'Richard Taylor','Physics',0),(10,'Laura Harris','Mathematics',0),(11,'Daniel Martin','Chemistry',0),(12,'Grace Anderson','English',0),(13,'William Thomas','Mathematics',0),(14,'Olivia Jackson','Physics',0),(15,'Ethan White','Chemistry',0),(16,'Ava Martinez','English',0),(17,'Logan Lee','CS',0),(18,'Sophia Taylor','Physics',0),(19,'Jackson Miller','Chemistry',0),(20,'Lily Harris','English',0),(21,'Benjamin Scott','CS',0),(22,'Chloe Allen','Mathematics',0),(23,'Christopher Hall','Chemistry',0),(24,'Mia Turner','English',0),(25,'Elijah Young','Mathematics',0),(26,'Avery Adams','Physics',0),(27,'Addison Clark','CS',0),(28,'Mason Turner','English',0),(29,'Scarlett Walker','Physics',0),(30,'Lucas Lewis','Mathematics',0),(31,'Hannah Parker','CS',0),(32,'Gabriel Wright','English',0),(33,'Aria Foster','Physics',0),(34,'Leo Morris','CS',0),(35,'Zoe Cooper','CS',0);
+/*!40000 ALTER TABLE `teacher_list` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-02-18 20:22:27
+CREATE DATABASE  IF NOT EXISTS `students` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `students`;
 -- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
 --
 -- Host: localhost    Database: students
@@ -548,4 +556,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-02-18 20:12:34
+-- Dump completed on 2024-02-18 20:22:27
